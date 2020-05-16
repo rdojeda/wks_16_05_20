80451f506f85bc6fdfaa6ce62c02efa0ec8b8ff5
rdojeda <ojeda.robertodaniel@gmail.com>
2020-05-16 13:52:35 -0300

    Ultimas correcciones

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const docs = express.static("docs")
 
 server.use( docs )
 
+server.get("index.html", function (request, response) {
+    response.end("Hola desde Node.JS + Express");
+
+})
 // server.post()
 
 server.listen(2000)
